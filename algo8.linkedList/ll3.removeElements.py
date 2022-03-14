53e839c32215b80160bfc55b677c5b7aa58bb191
badoil <soulish515151@gmail.com>
2022-03-14 13:56:20 +0900

    fix: ll3

--- algo8.linkedList/ll3.removeElements.py
+++ algo8.linkedList/ll3.removeElements.py
@@ -1,6 +1,26 @@
 # 203 
 # remove linked list elements
-# 여러 엘리먼트들 삭제하기
+# 지우고자 하는 val을 갖는 모든 엘리먼트 삭제하기
+
+# Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+# Example 1:
+# Input: head = [1,2,6,3,4,5,6], val = 6
+# Output: [1,2,3,4,5]
+
+# Example 2:
+# Input: head = [], val = 1
+# Output: []
+
+# Example 3:
+# Input: head = [7,7,7,7], val = 7
+# Output: []
+ 
+# Constraints:
+# The number of nodes in the list is in the range [0, 104].
+# 1 <= Node.val <= 50
+# 0 <= val <= 50
+
 
 from typing import List
 
@@ -17,7 +37,7 @@ def createNodeList(inList: List[int]) -> NodeList:
   lastNode = headNode
 
   for idx in range(1, len(inList)):
-    node = NodeList[idx]
+    node = NodeList(inList[idx])
     lastNode.next = node
     lastNode = node
 
@@ -28,6 +48,51 @@ def printNodes(node: NodeList):
   while crtNode is not None:
     print(crtNode.val, end=" ")
     crtNode= crtNode.next
+
+
+class ElementRemover:
+  def __init__(self,val):
+    self.__val = val
+
+  #arg_node data is not kept
+  def recursive(self, node: NodeList) -> NodeList:
+    if not node:
+      return None
+    next_node = self.recursive(node.next)
+    if node.val == self.__val:
+      return next_node
+    else:
+      node.next = next_node
+      return node
+  
+  #arg_node data is not kept
+  def iterative(self, node: NodeList) -> NodeList:    
+    dummy_head = NodeList(0)
+    dummy_head.next = node
+    
+    crnt_node = node
+    prev_node = dummy_head
+    while crnt_node:
+      if crnt_node.val == self.__val:
+        prev_node.next = crnt_node.next
+        crnt_node = crnt_node.next
+      else:
+        crnt_node = crnt_node.next
+        prev_node = prev_node.next
+    return dummy_head.next
+  
+head = createNodeList([1,3,5,7,3,1])
+printNodes(head)
+
+remover = ElementRemover(1)
+ret = remover.recursive(head)
+printNodes(ret)
+
+
+
+
+#####################################################################################
+
 
 
 class RemoveElements:
@@ -54,7 +119,7 @@ class RemoveElements:
     prevNode = dummyNode
     while crtNode is not None:
       if crtNode.val == self._val:
-        prevNode.next = crtNode.next    # 현재 노드 건너뛰어서 삭제
+        prevNode.next = crtNode.next    # 현재 노드 건너뛰어서 삭제, 이때 prevNode는 그대로
         crtNode = crtNode.next          
       else:
         crtNode = crtNode.next          # 현재노드 하나 증가
