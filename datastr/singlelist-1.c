79c1853a0030e87f5a461775335061656692af30
oneal.oh <oneal.oh@kakaomobility.com>
2022-10-29 20:52:49 +0900

    fix, singlelist-1

--- datastr/singlelist-1.c
+++ datastr/singlelist-1.c
@@ -12,13 +12,15 @@ int main() {
 
     NODE list[3] = { 0 };
 
-    list[0].next = &list[1];
-    list[1].next = &list[2];
-    list[2].next = NULL;
-
+    // 값 초기화
     list[0].a = 100;
     list[1].a = 200;
     list[2].a = 300;
+
+    // 연결 리스트 구조화
+    list[0].next = &list[1];
+    list[1].next = &list[2];
+    list[2].next = NULL;
 
     NODE* pHead = &list[0];
     while (pHead != NULL)
